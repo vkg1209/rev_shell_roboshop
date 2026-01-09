1ba5f7d3aac4cd949e6399de8876be0e3042e6b2
gvk12099 <vinayg1209@gmail.com>
2026-01-09 11:36:25 +0530

    rev_shell_roboshop

--- ec2_r53.sh
+++ ec2_r53.sh
@@ -38,7 +38,7 @@ do
                 "Type"             : "A",
                 "TTL"              : 1 ,
                 "ResourceRecords"  : [{
-                    "Value"         : "'${IP}'"
+                    "Value"         : "'$IP'"
                 }]
             }
         }]
